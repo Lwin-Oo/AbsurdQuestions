ff8192120dd44546ad49d2d627aa4e00d916da1c
Lwin Paing Oo <78170253+Lwin-Oo@users.noreply.github.com>
2023-11-22 22:51:09 -0600

    Update main.py

--- UserProfileManagement/main.py
+++ UserProfileManagement/main.py
@@ -1,58 +1,68 @@
-class User:
-    def __init__(self, username, name, email):
-        self.username = username
-        self.name = name
-        self.email = email
+class UserDatabase:
+    def __init__(self, file_path):
+        self.file_path = file_path
+        self.users = self.load_users()
 
-class UserManager:
-    def __init__(self):
-        self.users = []
+    def load_users(self):
+        try:
+            with open(self.file_path, 'r') as file:
+                users = {}
+                for line in file:
+                    user_data = line.strip().split(',')
+                    user_id = int(user_data[0])
+                    name = user_data[1]
+                    age = int(user_data[2])
+                    email = user_data[3]
+                    users[user_id] = {'name': name, 'age': age, 'email': email}
+                return users
+        except FileNotFoundError:
+            return {}
 
-    def insert(self, user):
-        self.users.append(user)
+    def save_users(self):
+        with open(self.file_path, 'w') as file:
+            for user_id, user_info in self.users.items():
+                file.write(f"ID: {user_id}\nName: {user_info['name']}\nAge: {user_info['age']}\nEmail: {user_info['email']}\n\n")
 
-    def find(self, username):
-        for user in self.users:
-            if user.username == username:
-                return user
+    def insert_user(self):
+        user_id = int(input("Enter user ID: "))
+        name = input("Enter user name: ")
+        age = int(input("Enter user age: "))
+        email = input("Enter user email: ")
 
-    def update(self, username, new_user_data):
-        for user in self.users:
-            if user.username == username:
-                user.name, user.email = new_user_data
-                return True
-        return False
+        if user_id not in self.users:
+            self.users[user_id] = {'name': name, 'age': age, 'email': email}
+            self.save_users()
+            print(f"User with ID {user_id} added successfully.")
+        else:
+            print(f"User with ID {user_id} already exists. Use update operation to modify the user.")
+
+    def update_user(self):
+        user_id = int(input("Enter user ID to update: "))
+        if user_id in self.users:
+            name = input(f"Enter new name for user {user_id}: ")
+            age = int(input(f"Enter new age for user {user_id}: "))
+            email = input(f"Enter new email for user {user_id}: ")
+
+            self.users[user_id]['name'] = name
+            self.users[user_id]['age'] = age
+            self.users[user_id]['email'] = email
+
+            self.save_users()
+            print(f"User with ID {user_id} updated successfully.")
+        else:
+            print(f"User with ID {user_id} does not exist. Use insert operation to add a new user.")
 
     def list_all_users(self):
-        return self.users
+        if not self.users:
+            print("No users in the database.")
+        else:
+            print("List of all users:")
+            for user_id, user_info in self.users.items():
+                print(f"User ID: {user_id}, Name: {user_info['name']}, Age: {user_info['age']}, Email: {user_info['email']}")
 
-def read_user_information(user_information):
-    users = []
-    with open(user_information, 'r') as file:
-        lines = file.readlines()
-        user_info = {}
-        for line in lines:
-            line = line.strip()
-            if line:
-                key, value = line.split(': ')
-                user_info[key] = value
-            else:
-                users.append(User(user_info['Username'], user_info['Name'], user_info['Email']))
-                user_info = {}
-    return users
-
-def write_user_information(user_information, users):
-    with open(user_information, 'w') as file:
-        for user in users:
-            file.write(f'Username: {user.username}\n')
-            file.write(f'Name: {user.name}\n')
-            file.write(f'Email: {user.email}\n')
-            file.write('\n')
-
-# Initialize the UserManager and read user information from the file
-user_manager = UserManager()
-user_information = 'user_information.txt'
-users = read_user_information(user_information)
+# Example usage:
+file_path = 'user_database.txt'
+user_db = UserDatabase(file_path)
 
 while True:
     print("1. Insert new user")
@@ -61,32 +71,19 @@ while True:
     print("4. Exit")
     
     choice = input("Enter your choice: ")
-    
+
     if choice == '1':
-        name_input = input("Name: ")
-        username_input = input("Username: ")
-        email_input = input("Email: ")
-        new_user = User(username_input, name_input, email_input)
-        user_manager.insert(new_user)
-        users.append(new_user)
-        write_user_information(user_information, users)
-        print("User inserted successfully.")
+        user_db.insert_user()
     
     elif choice == '2':
-        username = input("Enter the username of the user to update: ")
-        new_name = input("New name: ")
-        new_email = input("New email: ")
-        if user_manager.update(username, (new_name, new_email)):
-            write_user_information(user_information, users)
-            print("User updated successfully.")
-        else:
-            print(f"User with username '{username}' not found.")
-    
+        user_db.update_user()
+
     elif choice == '3':
-        all_users = user_manager.list_all_users()
-        for user in all_users:
-            print(f"Name: {user.name}, Username: {user.username}, Email: {user.email}")
-    
+        user_db.list_all_users()
+
     elif choice == '4':
-        print("Goodbye!")
+        print("Exiting the program. Goodbye!")
         break
+
+    else:
+        print("Invalid choice. Please enter a valid option.")
